c9b03e883984fbc4ffaa587674398124e9c5324b
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-05-03 09:21:07 +0100

    Generates project

---
+++ lib/rps/application.ex
@@ -0,0 +1,20 @@
+defmodule Rps.Application do
+  # See https://hexdocs.pm/elixir/Application.html
+  # for more information on OTP Applications
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    # List all child processes to be supervised
+    children = [
+      # Starts a worker by calling: Rps.Worker.start_link(arg)
+      # {Rps.Worker, arg},
+    ]
+
+    # See https://hexdocs.pm/elixir/Supervisor.html
+    # for other strategies and supported options
+    opts = [strategy: :one_for_one, name: Rps.Supervisor]
+    Supervisor.start_link(children, opts)
+  end
+end
