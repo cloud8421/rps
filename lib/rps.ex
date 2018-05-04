4f56947eb65472ad9053c8351360b8362410de5d
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-05-04 12:33:50 +0100

    Extends public api

--- lib/rps.ex
+++ lib/rps.ex
@@ -21,15 +21,27 @@ defmodule Rps do
   Starts a new game given a game id.
   """
   @spec start_game() :: {:ok, Rps.Game.id(), pid} | {:error, {:already_started, pid}}
-  def start_game(id) do
-    case Rps.Game.Supervisor.start_game(id) do
+  def start_game(game_id) do
+    case Rps.Game.Supervisor.start_game(game_id) do
       {:ok, pid} ->
-        {:ok, id, pid}
+        {:ok, game_id, pid}
 
       error ->
         error
     end
   end
+
+  @doc """
+  Joins a game given a game id and a player id.
+  """
+  @spec join(Rps.Game.id(), Rps.Game.player_id()) :: Rps.Game.Session.on_join()
+  def join(game_id, player_id), do: Rps.Game.Session.join(game_id, player_id)
+
+  @doc """
+  Joins a game given a game id and a player id.
+  """
+  @spec move(Rps.Game.id(), Rps.Game.player_id(), Rps.Rules.move()) :: Rps.Game.Session.on_move()
+  def move(game_id, player_id, move), do: Rps.Game.Session.move(game_id, player_id, move)
 
   defp generate_id do
     :rand.uniform(1_000_000_000)
