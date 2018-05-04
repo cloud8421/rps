29f07d22a9ed829e772d8248df41c07c8da25947
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-05-04 09:59:08 +0100

    Up to 2 players can join a game

--- lib/rps/game.ex
+++ lib/rps/game.ex
@@ -1,24 +1,48 @@
 defmodule Rps.Game do
   use GenServer
 
-  @type id :: String.t
+  @type id :: String.t()
+  @type player_id :: String.t()
+
+  defstruct id: nil,
+            players: MapSet.new(),
+            moves: %{}
 
   # PUBLIC API
 
   @spec start_link(id()) :: GenServer.on_start()
-  def start_link(id) do
-    GenServer.start_link(__MODULE__, %{}, name: via(id))
+  def start_link(game_id) do
+    GenServer.start_link(__MODULE__, game_id, name: via(game_id))
+  end
+
+  @spec join(id(), player_id()) :: :ok | {:error, :game_full} | no_return
+  def join(game_id, player_id) do
+    GenServer.call(via(game_id), {:join, player_id})
   end
 
   # CALLBACKS
 
-  def init(args) do
-    {:ok, args}
+  def init(game_id) do
+    state = %__MODULE__{id: game_id}
+    {:ok, state}
+  end
+
+  def handle_call({:join, player_id}, _from, state) do
+    if game_full?(state) do
+      {:reply, {:error, :game_full}, state}
+    else
+      new_state = %{state | players: MapSet.put(state.players, player_id)}
+      {:reply, :ok, new_state}
+    end
   end
 
   # PRIVATE
 
-  defp via(id) do
-    {:via, Registry, {Registry.Game, id}}
+  defp via(game_id) do
+    {:via, Registry, {Registry.Game, game_id}}
+  end
+
+  defp game_full?(state) do
+    MapSet.size(state.players) >= 2
   end
 end
