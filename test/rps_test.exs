0388f939045d66ef7164b1774fb701981cdf41d0
Claudio Ortolina <claudio.ortolina@erlang-solutions.com>
2018-05-04 12:46:30 +0100

    Adds simple top-level test

---
+++ test/rps_test.exs
@@ -0,0 +1,21 @@
+defmodule Rps.Test do
+  use ExUnit.Case
+
+  test "example game session" do
+    {:ok, game_id, _pid} = Rps.start_game()
+
+    # JOIN
+
+    assert :ok == Rps.join(game_id, "player-one")
+    assert :ok == Rps.join(game_id, "player-two")
+    assert {:error, :game_full} == Rps.join(game_id, "player-three")
+
+    # MOVES
+
+    assert :ok == Rps.move(game_id, "player-one", :rock)
+    assert :ok == Rps.move(game_id, "player-two", :paper)
+    assert :ok == Rps.move(game_id, "player-one", :scissors)
+    assert {:error, :invalid_move} == Rps.move(game_id, "player-two", :fire)
+    assert {:error, :not_in_game} == Rps.move(game_id, "player-three", :rock)
+  end
+end
